e3b5fb64ef4552b80063467a6224a852674abf08
unknown <72319744+Torchikaii@users.noreply.github.com>
2025-06-26 23:48:30 +0300

    coordinate system and display system bugfix

--- main.py
+++ main.py
@@ -3,140 +3,53 @@ from PIL import Image, ImageTk
 import json
 
 
-def rgb(r, g ,b):
-    tuple = (r, g, b)
-    return "#%02x%02x%02x" % tuple
+def rgb(r, g, b):
+    return "#%02x%02x%02x" % (r, g, b)
 
 
-class App(tk.Tk):
-
-
-    position_list = [[0.1, 0.1]]  # user tab positions
-    step_x = 0.15
-    step_y = 0.4
-    ind = 0  # total number of users
-    user_amt_row = 0  # how many users in 1 row
-    button_color = rgb(0, 126, 100)
-
-
-    def __init__(self):
-        tk.Tk.__init__(self)
-
-        self.title("Notebook")
-        self.geometry("1366x786")
-
-        self.label = tk.Label(self)
-        self.imgg = Image.open(r"images\backgroundas.png")
-        self.label.img = ImageTk.PhotoImage(self.imgg)
-        self.label['image'] = self.label.img
-        self.label.place(relwidth=1, relheight=1, relx=0, rely=0)
-
-
-        self.laukelis_add = tk.Frame(self, bg=rgb(49, 164, 125))
-        self.laukelis_add.place(
-
-            relwidth=0.1464,
-            relheight=0.395,
-            relx=self.position_list[0][0],
-            rely=self.position_list[0][1]
-        )
-
-        self.add_button = tk.Button(self.laukelis_add, command=self.new_user,
-        bg="blue"
-        )
-
-        self.add_button.place(relx=0.5, rely=0.5, anchor="center")
-        self.load_users()
-
-    # coordinates of widgets
-    @classmethod
-    def coords(cls):
-        cls.user_amt_row += 1
-
-        if cls.user_amt_row == 6:
-            cls.position_list[cls.ind][0] = 0.1
-            cls.position_list.append([cls.position_list[cls.ind][0], cls.position_list[cls.ind][1] + cls.step_y])
-            cls.user_amt_row = 0
-        else:
-            cls.position_list.append([cls.position_list[cls.ind][0] + cls.step_x, cls.position_list[cls.ind][1]])
-        cls.ind += 1
-
-
-    def new_user(self):
-
-        self.coords()
-        self.laukelis_add.place_forget()
-
-        self.laukelis_add.place(
-        relwidth=0.1464,
-        relheight=0.395,
-        relx=self.position_list[self.ind][0],
-        rely=self.position_list[self.ind][1]
-        )
-
-        self.user = User_frame()
-        self.user.place(
-        relx=self.position_list[self.ind - 1][0],
-        rely=self.position_list[self.ind - 1][1],
-        relwidth=0.1464,
-        relheight=0.395
-    )
-
-    def load_users(self):
-        self.data = Json_writer.read_json(self, "data.json")
-        for _ in range(len(self.data["users"])):
-            self.new_user()
-
-
-
-
-
-class Json_writer:
-
-    def write_json(self, data, filename):
+class JsonWriter:
+    @staticmethod
+    def write_json(data, filename):
         with open(filename, "w") as f:
             json.dump(data, f, indent=4)
 
-
-    def read_json(self, filename):
+    @staticmethod
+    def read_json(filename):
         with open(filename) as f:
-            self.data = json.load(f)
-            return self.data
+            return json.load(f)
+
+    @staticmethod
+    def add_user(filename, json_element):
+        data = JsonWriter.read_json(filename)
+        data["users"].append(json_element)
+        JsonWriter.write_json(data, filename)
+
+    @staticmethod
+    def delete_user(filename, index):
+        data = JsonWriter.read_json(filename)
+        del data["users"][index]
+        JsonWriter.write_json(data, filename)
 
 
-    def add_user(self, filename, json_element):
-        self.data = Json_writer.read_json(self, filename)
-        self.data["users"].append(json_element)
-        Json_writer.write_json(self, self.data, filename)
-
-
-    def delete_user(self, filename, index):
-        self.data = Json_writer.read_json(self, filename)
-        del self.data["users"][index]
-        Json_writer.write_json(self, self.data, filename)
-
-
-
-
-# Keeping track of frames
-class User_frame(tk.Frame):
-
-    counter = 0
-    widgets = []
-
-    def __init__(self, *args, **kwargs):
-        tk.Frame.__init__(self, *args, **kwargs)
-
-        User_frame.counter += 1
-        self.widgets.append(self)
-        #class variables
+class UserFrame(tk.Frame):
+    def __init__(self, master, user_data=None, on_delete=None, on_update=None, *args, **kwargs):
+        super().__init__(master, *args, **kwargs)
+        self.on_delete = on_delete
+        self.on_update = on_update
+        self.user_data = user_data or {
+            "vardas": "",
+            "pavarde": "",
+            "adresas": "",
+            "el_pastas": "",
+            "tel_nr": ""
+        }
+        self["bg"] = rgb(49, 164, 125)
+        label_color = rgb(49, 164, 125)
+        entry_color = rgb(0, 190, 151)
         self.dist = 0.05
         self.dist_xent = 0.4
-
-        self["bg"] = rgb(49, 164, 125)
-
-        label_color = rgb(49, 164, 125)
-        self.vardas = tk.Label(self, text="Vardas:", bg=label_color)  # data[i]["vardas"]
+        # Labels
+        self.vardas = tk.Label(self, text="Vardas:", bg=label_color)
         self.vardas.place(relx=self.dist, rely=0.2)
         self.pavarde = tk.Label(self, text="Pavardė:", bg=label_color)
         self.pavarde.place(relx=self.dist, rely=0.3)
@@ -146,48 +59,57 @@ class User_frame(tk.Frame):
         self.email.place(relx=self.dist, rely=0.5)
         self.numeris = tk.Label(self, text="Tel. nr.:", bg=label_color)
         self.numeris.place(relx=self.dist, rely=0.6)
-
-        entry_color = rgb(0, 190, 151)
-        self.ent_vardas = tk.Entry(self, state="normal", bg=entry_color)  # data[i]["vardas"]
+        # Entries
+        self.ent_vardas = tk.Entry(self, state="disabled", bg=entry_color)
         self.ent_vardas.place(relx=self.dist_xent, rely=0.2)
-        self.ent_pavarde = tk.Entry(self, state="normal", bg=entry_color)
+        self.ent_pavarde = tk.Entry(self, state="disabled", bg=entry_color)
         self.ent_pavarde.place(relx=self.dist_xent, rely=0.3)
-        self.ent_adresas = tk.Entry(self, state="normal", bg=entry_color)
+        self.ent_adresas = tk.Entry(self, state="disabled", bg=entry_color)
         self.ent_adresas.place(relx=self.dist_xent, rely=0.4)
-        self.ent_email = tk.Entry(self, state="normal", bg=entry_color)
+        self.ent_email = tk.Entry(self, state="disabled", bg=entry_color)
         self.ent_email.place(relx=self.dist_xent, rely=0.5)
-        self.ent_numeris = tk.Entry(self, state="normal", bg=entry_color)
+        self.ent_numeris = tk.Entry(self, state="disabled", bg=entry_color)
         self.ent_numeris.place(relx=self.dist_xent, rely=0.6)
-
-        self.b_edit = tk.Button(self, text="edit", command=self.edit_b_command,
-        bg=App.button_color)
-        self.b_edit_img = Image.open(r"images\edit-mazas.ico")
+        # Fill entries if data is provided
+        self.set_fields(self.user_data)
+        # Buttons
+        self.b_edit = tk.Button(self, command=self.edit_b_command, bg=App.button_color)
+        self.b_edit_img = Image.open(r"images/edit-mazas.ico")
         self.b_edit.img = ImageTk.PhotoImage(self.b_edit_img)
         self.b_edit.config(image=self.b_edit.img)
         self.b_edit.place(relx=0.5)
-
-        self.b_del = tk.Button(self, text="delete", command=self.delete_widget,
-        bg=App.button_color)
-        self.b_del_img = Image.open(r"images\Delete-mazas.ico")
+        self.b_del = tk.Button(self, command=self.delete_widget, bg=App.button_color)
+        self.b_del_img = Image.open(r"images/Delete-mazas.ico")
         self.b_del.img = ImageTk.PhotoImage(self.b_del_img)
         self.b_del.config(image=self.b_del.img)
         self.b_del.place(relx=0.75)
-
-        self.b_ok = tk.Button(self, text="OK", command=self.b_ok_command,
-        bg=App.button_color)
-        self.b_ok_img = Image.open(r"images\OK-mazas.ico")
+        self.b_ok = tk.Button(self, command=self.b_ok_command, bg=App.button_color)
+        self.b_ok_img = Image.open(r"images/OK-mazas.ico")
         self.b_ok.img = ImageTk.PhotoImage(self.b_ok_img)
         self.b_ok.config(image=self.b_ok.img)
+        # self.b_ok is not placed initially
 
-
-
-    def deactivate(self):
-        self.ent_vardas["state"] = "disabled"
-        self.ent_pavarde["state"] = "disabled"
-        self.ent_adresas["state"] = "disabled"
-        self.ent_email["state"] = "disabled"
-        self.ent_numeris["state"] = "disabled"
-
+    def set_fields(self, data):
+        self.ent_vardas.config(state="normal")
+        self.ent_pavarde.config(state="normal")
+        self.ent_adresas.config(state="normal")
+        self.ent_email.config(state="normal")
+        self.ent_numeris.config(state="normal")
+        self.ent_vardas.delete(0, tk.END)
+        self.ent_vardas.insert(0, data.get("vardas", ""))
+        self.ent_pavarde.delete(0, tk.END)
+        self.ent_pavarde.insert(0, data.get("pavarde", ""))
+        self.ent_adresas.delete(0, tk.END)
+        self.ent_adresas.insert(0, data.get("adresas", ""))
+        self.ent_email.delete(0, tk.END)
+        self.ent_email.insert(0, data.get("el_pastas", ""))
+        self.ent_numeris.delete(0, tk.END)
+        self.ent_numeris.insert(0, data.get("tel_nr", ""))
+        self.ent_vardas.config(state="disabled")
+        self.ent_pavarde.config(state="disabled")
+        self.ent_adresas.config(state="disabled")
+        self.ent_email.config(state="disabled")
+        self.ent_numeris.config(state="disabled")
 
     def activate(self):
         self.ent_vardas["state"] = "normal"
@@ -196,36 +118,137 @@ class User_frame(tk.Frame):
         self.ent_email["state"] = "normal"
         self.ent_numeris["state"] = "normal"
 
+    def deactivate(self):
+        self.ent_vardas["state"] = "disabled"
+        self.ent_pavarde["state"] = "disabled"
+        self.ent_adresas["state"] = "disabled"
+        self.ent_email["state"] = "disabled"
+        self.ent_numeris["state"] = "disabled"
 
     def edit_b_command(self):
         self.b_edit.place_forget()
         self.b_ok.place(relx=0.5)
         self.activate()
 
-
     def b_ok_command(self):
         self.b_ok.place_forget()
         self.b_edit.place(relx=0.5)
-
-
-        self.temp_dict = {
-
+        self.deactivate()
+        # Update user data
+        self.user_data = {
             "vardas": self.ent_vardas.get(),
             "pavarde": self.ent_pavarde.get(),
             "adresas": self.ent_adresas.get(),
-            "el_pastas" : self.ent_email.get(),
+            "el_pastas": self.ent_email.get(),
             "tel_nr": self.ent_numeris.get()
         }
-
-
-        Json_writer.add_user(self, "data.json", self.temp_dict)
-        self.deactivate()
-
+        if self.on_update:
+            self.on_update(self)
 
     def delete_widget(self):
+        if self.on_delete:
+            self.on_delete(self)
 
-        self.place_forget()
-        Json_writer.delete_user(self, "data.json", 0)
+
+class App(tk.Tk):
+    button_color = rgb(0, 126, 100)
+    step_x = 0.15
+    step_y = 0.4
+    max_per_row = 6
+
+    def __init__(self):
+        super().__init__()
+        self.title("Notebook")
+        self.geometry("1366x786")
+        # Background
+        self.label = tk.Label(self)
+        self.imgg = Image.open(r"images/backgroundas.png")
+        self.label.img = ImageTk.PhotoImage(self.imgg)
+        self.label['image'] = self.label.img
+        self.label.place(relwidth=1, relheight=1, relx=0, rely=0)
+        # User frames
+        self.user_frames = []
+        # Add button frame
+        self.add_frame = tk.Frame(self, bg=rgb(49, 164, 125))
+        self.add_button = tk.Button(self.add_frame, command=self.add_user, bg="blue")
+        self.add_img = Image.open(r"images/add-mazas.ico")
+        self.add_button.img = ImageTk.PhotoImage(self.add_img)
+        self.add_button.config(image=self.add_button.img)
+        self.add_button.place(relx=0.5, rely=0.5, anchor="center")
+        # Load users from file
+        self.data_file = "data.json"
+        self.load_users()
+
+    def get_position(self, idx):
+        row = idx // self.max_per_row
+        col = idx % self.max_per_row
+        relx = 0.1 + col * self.step_x
+        rely = 0.1 + row * self.step_y
+        return relx, rely
+
+    def render_widgets(self):
+        # Remove all user frames and add_frame from UI
+        for frame in self.user_frames:
+            frame.place_forget()
+        self.add_frame.place_forget()
+        # Place user frames
+        for idx, frame in enumerate(self.user_frames):
+            relx, rely = self.get_position(idx)
+            frame.place(relx=relx, rely=rely, relwidth=0.1464, relheight=0.395)
+        # Place add button after last user (always visible)
+        relx, rely = self.get_position(len(self.user_frames))
+        self.add_frame.place(relx=relx, rely=rely, relwidth=0.1464, relheight=0.395)
+
+    def load_users(self):
+        # Remove any existing user frames from UI and memory
+        for frame in self.user_frames:
+            frame.place_forget()
+            frame.destroy()
+        self.user_frames.clear()
+        try:
+            data = JsonWriter.read_json(self.data_file)
+            users = data.get("users", [])
+        except Exception:
+            users = []
+        for user_data in users:
+            frame = UserFrame(self, user_data=user_data, on_delete=self.delete_user, on_update=self.update_user)
+            self.user_frames.append(frame)
+        self.render_widgets()
+
+    def add_user(self):
+        # Add a new user with empty fields
+        new_user = {
+            "vardas": "",
+            "pavarde": "",
+            "adresas": "",
+            "el_pastas": "",
+            "tel_nr": ""
+        }
+        frame = UserFrame(self, user_data=new_user, on_delete=self.delete_user, on_update=self.update_user)
+        self.user_frames.append(frame)
+        self.render_widgets()
+        # Ensure the new frame is visible and editable
+        frame.edit_b_command()
+
+    def delete_user(self, frame):
+        if frame in self.user_frames:
+            self.user_frames.remove(frame)
+            frame.place_forget()
+            frame.destroy()
+            self.save_all_users()
+            self.render_widgets()
+        else:
+            # Defensive: ignore if frame is not in list
+            pass
+
+    def update_user(self, frame):
+        # Save all users when one is updated
+        self.save_all_users()
+
+    def save_all_users(self):
+        users = [frame.user_data for frame in self.user_frames]
+        data = {"users": users}
+        JsonWriter.write_json(data, self.data_file)
 
 
 if __name__ == "__main__":
